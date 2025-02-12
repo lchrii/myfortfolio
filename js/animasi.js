44ba7bc0e610e279f1eb0cd738d97c98eb474f92
lchrii <meliala366m12@gmail.com>
2025-02-12 18:42:25 +0700

    first commit

---
+++ js/animasi.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const sections = document.querySelectorAll("section");
+
+    function revealSections() {
+        sections.forEach((section) => {
+            const sectionTop = section.getBoundingClientRect().top;
+            if (sectionTop < window.innerHeight - 100) {
+                section.classList.add("show");
+            }
+        });
+    }
+
+    window.addEventListener("scroll", revealSections);
+    revealSections();
+});
